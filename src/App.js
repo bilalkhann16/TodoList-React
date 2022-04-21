19731e74504effa853c5fd66cde9d9a81dac79a4
Bilal K <lalbikhan2014@gmail.com>
2022-04-22 01:02:12 +0500

    first commit - setting up the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// Importing components.
+import Form from "./components/form"
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1> Bilal's Todo List.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
